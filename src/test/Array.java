45666a19a56fc8fcdb1695eee694eb36f70db0e1
abdullah.bahir <abdullah.bahir2018@gmail.com>
2023-02-15 21:26:08 -0600

    simple array class

---
+++ src/test/Array.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String [] arr = new String[3];
+     arr[0]= "Kabul";
+     arr[1]= "Khost";
+     arr[2]="Parwan";
+     System.out.println(arr[0]);
+     System.out.println(arr[1]);
+     System.out.println(arr[2]);
+	}
+
+}
