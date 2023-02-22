5440657fd766c0dac64fa000803db6b21429712d
Abdullah Bahir <125522215+abdullahbahir@users.noreply.github.com>
2023-02-22 17:06:13 -0600

    Update Array.java
    this is for pull practice

--- src/test/Array.java
+++ src/test/Array.java
@@ -4,15 +4,15 @@ public class Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     String [] arr = new String[4];
+     String [] arr = new String[3];
      arr[0]= "Kabul";
      arr[1]= "Khost";
      arr[2]="Parwan";
-     arr[3]="Kapisa";
+    
      System.out.println(arr[0]);
      System.out.println(arr[1]);
      System.out.println(arr[2]);
-     System.out.println(arr[3]);	
+	
 	}
 
 }
