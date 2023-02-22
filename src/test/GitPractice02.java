8bdc2468fb9c19c341bbe90ffd05b4dc69e62d8c
Abdullah Bahir <125522215+abdullahbahir@users.noreply.github.com>
2023-02-21 19:10:31 -0600

    Update GitPractice02.java
    it is pull practice

--- src/test/GitPractice02.java
+++ src/test/GitPractice02.java
@@ -4,7 +4,7 @@ public class GitPractice02 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("this is for practice");
+		System.out.println("this is for pul practice");
 
 	}
 
