a6652470b36aa33be4a606d7543e2a5a45398da5
Transfromers <abdullah.bahir2018@gmail.com>
2023-02-21 18:50:52 -0600

    this is gitPractice02

--- src/test/ForLoop.java
+++ src/test/ForLoop.java
@@ -4,7 +4,7 @@ public class ForLoop {
 
 	public static void main(String[] args) {
      
-		for(int i =0; i<10; i++) {
+		for(int i =2; i<10; i++) {
 			System.out.println(i);
 			
 		}
---
+++ src/test/GitPractice02.java
@@ -0,0 +1,11 @@
+package test;
+
+public class GitPractice02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is for practice");
+
+	}
+
+}
